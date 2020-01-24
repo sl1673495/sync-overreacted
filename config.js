954b861c1376c266071855d0f61c659be5a75f94
ssh <949589855@qq.com>
2020-01-24 13:44:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,13 @@ const config = {
   mdDir,
   // 用于更改标题上的用户信息
   username: 'ssh',
-  // 用于同步github的博客
+  // 你的掘金主页
   juejin: 'https://juejin.im/user/5b13f11d5188257da1245183',
+  // 用于同步github的博客
   repo: {
+    // 你的github用户名
     owner: 'sl1673495',
+    // 博客的仓库名 会从这个仓库拉取issues
     name: 'blogs',
   },
   // 可选 如果申请了github Oauth app的话
