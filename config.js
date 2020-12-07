e3ff2cba0b9569478371ee13d6cc4fade6160be6
sl1673495 <949589855@qq.com>
2020-12-07 10:52:30 +0800

    fix: config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-
 const mdDir = path.resolve(__dirname, './src/pages');
+const { client_id, client_secret } = require('./.github');
 
 const config = {
   mdDir,
@@ -17,8 +17,8 @@ const config = {
   },
   // 可选 如果申请了github Oauth app的话
   // 可以填写用于取消github请求限制
-  client_id: '6a1ff2700790b55ade14',
-  client_secret: 'bddc92e3fc965fc285dd8b4e98bdd58640791b2c',
+  client_id,
+  client_secret,
 };
 
 const githubUrl = `https://github.com/${config.repo.owner}`;
